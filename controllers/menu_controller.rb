705e1664c659c62232438c9142f859817adb3a12
EC2 Default User <ec2-user@ip-172-31-10-250.us-east-2.compute.internal>
2018-02-13 22:18:13 +0000

    Assignment 5 Address Bloc: complete

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -10,12 +10,13 @@ class MenuController
     def main_menu
         
         puts "Main Menu - #{address_book.entries.count} entries"
-        puts "1 - View all entries"
-        puts "2 - Create an entry"
-        puts "3 - Search for an entry"
-        puts "4 - Import entries from CSV"
-        puts "5 - Exit"
-        print "Enter your selection: "
+        puts "1 - View all Entries"
+        puts "2 - View Entry by Number"
+        puts "3 - Create an Entry"
+        puts "4 - Search for an Entry"
+        puts "5 - Import Entries from CSV"
+        puts "6 - Exit"
+        print "Enter Your election: "
         
         selection = gets.to_i
 
@@ -27,24 +28,28 @@ class MenuController
                 main_menu
             when 2
                 system "clear"
-                create_entry
+                view_entry_number
                 main_menu
             when 3
                 system "clear"
-                search_entries
+                create_entry
                 main_menu
             when 4
                 system "clear"
-                read_csv
+                search_entries
                 main_menu
             when 5
+                system "clear"
+                read_csv
+                main_menu
+            when 6
                 puts "Good-bye!"
                 
                 exit(0)
                 
             else
                 system "clear"
-                puts "Sorry, that is not a valud input"
+                puts "Sorry, that is not a valid input"
                 main_menu
         end
     end
@@ -64,10 +69,39 @@ class MenuController
         
     end
     
+    def view_entry_number
+        
+        if address_book.entries.empty? 
+            system "clear"
+            puts "Sorry, there are no entries to select." 
+        end
+        
+        print "Please enter a number between 1 and #{address_book.entries.length}: "
+        number = gets.chomp
+        
+        
+        if !/\A\d+\z/.match(number)
+            system "clear"
+            puts "Please enter a number."
+            view_entry_number
+            
+        elsif number.to_i > address_book.entries.length
+            system "clear"
+            view_entry_number
+        
+        else
+            system "clear"
+            puts address_book.entries[number.to_i - 1]
+           
+        end
+        
+    end
+    
     def create_entry
         
         system "clear"
         puts "New AddressBloc Entry"
+        
         
         print "Name: "
         name = gets.chomp
